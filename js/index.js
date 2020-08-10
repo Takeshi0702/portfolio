c16aeda00c540157d3f008ac327108ce7d07868b
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-08-10 18:49:44 +0900

    portfolio作成

--- js/index.js
+++ js/index.js
@@ -4,16 +4,15 @@ window.addEventListener( "scroll" ,function(){
   //スクロールの高さを取得
   let scroll = window.pageYOffset;
   
-  if( scroll > 4000 ){ 
-    document.body.style.backgroundColor = '#FFC400';
-  }else if( scroll > 3000 ){
-    document.body.style.backgroundColor = '#43A047';
-  }else if( scroll > 2000 ){
-    document.body.style.backgroundColor = '#FF6F00';
-  }else if( scroll > 1000 ){
-    document.body.style.backgroundColor = '#0091EA';
+  if( scroll > 2600 ){
+    document.body.style.backgroundColor = '#FF8856';
+  }else if( scroll > 2250 ){
+    document.body.style.backgroundColor = '#ffffff';
+  }else if( scroll > 1440 ){
+    document.body.style.backgroundColor = '#40E0D0';
+  }else if( scroll > 650 ){
+    document.body.style.backgroundColor = '#00BFFF';
   }else{
-    document.body.style.backgroundColor = '#FF4081';
+    document.body.style.backgroundColor = '#CC66FF';
   }
-  
 })
