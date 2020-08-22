3a925b10c1ea3bb6fb2a844b5fea46a6529a22de
Sasaki72 <samurai.engineer.cramschool@gmail.com>
2020-08-22 10:48:31 +0900

    portfolio作成

--- js/index.js
+++ js/index.js
@@ -16,3 +16,31 @@ window.addEventListener( "scroll" ,function(){
     document.body.style.backgroundColor = '#CC66FF';
   }
 })
+
+
+
+window.onscroll = onScroll;
+function onScroll() {
+    var removeActiveClass = function (elements) {
+        for (var i = 0; i < elements.length; ++i) {
+            elements[i].classList.remove('active');
+        }
+    }
+    var anchors = document.querySelectorAll('#menu-center a');
+    var previousRefElement = null;
+    for (var i = 0; i < anchors.length; ++i) {
+        var currentRefElement = document.getElementById(anchors[i].getAttribute('href').substring(1));
+        var currentRefElementTop = currentRefElement.getBoundingClientRect().top;
+        if (currentRefElementTop <= 0) {
+            previousRefElement = anchors[i];
+            if (i == anchors.length - 1) {
+                emoveActiveClass(anchors);
+                anchors[i].classList.add("active");
+            }
+        } else {
+            removeActiveClass(anchors);
+            previousRefElement.classList.add("active");
+            break;
+        }
+    }
+}
